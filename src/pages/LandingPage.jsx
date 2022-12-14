fd2059d171f9c313f75591e7ac2dae67e9fe3b20
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-14 17:21:14 +0300

    style the footer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Logo from '../data/images/Logo.svg';
 import HowToPlayPrompt from '../data/images/HowToPlayPrompt.png';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
 	return (
-		<div className='w-screen h-screen bg-landing-page bg-no-repeat bg-cover bg-center'>
+		<div className='relative w-screen h-screen bg-landing-page bg-no-repeat bg-cover bg-center'>
 			<header>
 				<div>
 					<img
@@ -38,11 +39,18 @@ const LandingPage = () => {
 					</div>
 				</section>
 			</main>
-			<footer>
-				<nav>
-					<p>Info Bank</p>
-					<p>About Recarnate</p>
-					<p>Team</p>
+
+			<footer className='bg-footer-bg-color w-full fixed left-0 bottom-0 text-white flex justify-between items-center px-12 py-5 text-sm'>
+				<nav className='flex justify-between gap-10'>
+					<p>
+						<Link to='/info'>Info Bank</Link>
+					</p>
+					<p>
+						<Link to='/about'>About Recarnate</Link>
+					</p>
+					<p>
+						<Link to='/team'>Team</Link>
+					</p>
 				</nav>
 				<div>
 					<p>&#169;Copyright 2022. All rights reserved.</p>
