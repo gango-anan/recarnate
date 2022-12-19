8a20e0133413427598f04f65ab606e48c8c4c1e1
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-19 17:58:05 +0300

    update custom background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
 				'landing-page': "url('/src/data/images/bg_landing_page.svg')",
 			},
 			backgroundColor: {
-				'recarnate-bg-green': '#158315',
-				'secondary-color-light': '#FFB703',
-				'secondary-color-dark': '#FFC73B',
 				'modal-background': 'rgba(0, 0, 0, 0.7)',
+				'recarnate-pink': '#FB5F5F',
+				'recarnate-primary-300': '#1DB51D',
+				'recarnate-primary-400': '#158315',
+				'recarnate-secondary-400': '#FFB703',
+				'recarnate-secondary-500': '#FEA302',
 			},
 			colors: {
 				'play-btn-font-color': '#2A1C02',
