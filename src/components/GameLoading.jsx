e7214298d6582172a15199aefe6d99c51c053b48
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-29 03:26:24 +0300

    update backgorund color and increase font-size of animating text

--- src/components/GameLoading.jsx
+++ src/components/GameLoading.jsx
@@ -4,31 +4,31 @@ import '../styles/GameLoading.css';
 
 const GameLoading = () => {
 	return (
-		<div className='w-screen h-screen flex flex-col justify-end items-center'>
+		<div className='w-screen h-screen flex flex-col justify-end items-center bg-recarnate-bg'>
 			<div className='logo-animation'>
 				<img
 					src={LogoInitial}
 					alt='Logo'
 				/>
 			</div>
-			<div className='logo-hidder flex flex-col justify-start items-center h-1/2 w-full bg-white z-10'>
+			<div className='logo-hidder flex flex-col justify-start items-center h-1/2 w-full bg-recarnate-bg z-10'>
 				<div className='first-text opacity-1 inline-block overflow-hidden whitespace-nowrap'>
-					<span className='text-2xl font-bold text-recarnate-neutral-800'>
+					<span className='text-4xl font-bold text-recarnate-neutral-800'>
 						The fate of your trash rests in your hands
 					</span>
 				</div>
 				<div className='second-text opacity-0 inline-block overflow-hidden whitespace-nowrap'>
-					<span className='text-2xl font-bold text-recarnate-primary-font-500'>
+					<span className='text-4xl font-bold text-recarnate-primary-font-500'>
 						No, seriously... It does
 					</span>
 				</div>
 				<div className='third-text opacity-0 inline-block overflow-hidden whitespace-nowrap'>
-					<span className='text-2xl font-bold text-recarnate-secondary-font-500'>
+					<span className='text-4xl font-bold text-recarnate-secondary-font-500'>
 						What will it be?
 					</span>
 				</div>
 				<div className='fourth-text opacity-0 inline-block overflow-hidden whitespace-nowrap'>
-					<span className='text-2xl font-bold text-recarnate-neutral-800'>
+					<span className='text-4xl font-bold text-recarnate-neutral-800'>
 						Death 💀 or recarnation🌿?
 					</span>
 				</div>
