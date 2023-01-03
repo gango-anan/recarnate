a42df424b7373a126335fa2cb26e524321ccaf7e
Galiwango Ananiya <galiwango101@gmail.com>
2023-01-03 04:10:23 +0300

    add functionality to drag an item from the item picker area to the respective container

--- src/pages/GameBoard.jsx
+++ src/pages/GameBoard.jsx
@@ -9,6 +9,7 @@ import TreeIcon from '../data/images/TreeIcon.svg';
 import '../styles/GameBoard.css';
 import ItemsCarousel from './ItemsCarousel';
 import ToolTip from './ToolTip';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const GameBoard = () => {
 	const { gameInfoBtn, randomizeBtn, soundBtn, failureBtn, searchBtn } =
@@ -99,13 +100,6 @@ const GameBoard = () => {
 					/>
 				)}
 			</div>
-			<div className='absolute bulk-pickup-track max-w-280'>
-				<DisposalBin
-					content={`${bulkPickup.name} Truck`}
-					direction='top'
-					icon={bulkPickup.icon}
-				/>
-			</div>
 			<div className='w-full absolute road'>
 				<img
 					src={Road}
@@ -113,57 +107,164 @@ const GameBoard = () => {
 					className='w-screen'
 				/>
 			</div>
-			{selectedItem.length > 0 && !isCarouselOpen && (
-				<div className='absolute item-picker w-36 h-36 flex flex-col items-center justify-center'>
-					<img
-						src={selectedItem[0].image}
-						alt='item'
-						className='w-100 h-auto'
-					/>
-					<span className='text-white text-sm'>{selectedItem[0].name}</span>
-				</div>
-			)}
-			<div className='w-full flex justify-between absolute bottom-3 left-0'>
-				<div className='repurpose-box w-1/4 mx-auto'>
-					<div className='w-3/4 -mt-10'>
-						<DisposalBin
-							content={`${repurposeBin.name} Box`}
-							direction='top'
-							icon={repurposeBin.icon}
-						/>
-					</div>
-				</div>
-				<div className='w-1/2 flex gap-14 justify-end items-baseline pr-20'>
-					<div className='trash-bin'>
-						<DisposalBin
-							content={`${trashBin.name} Bin`}
-							direction='top'
-							icon={trashBin.icon}
-						/>
-					</div>
-					<div className='recycle-bin'>
-						<DisposalBin
-							content={`${recycleBin.name} Bin`}
-							direction='top'
-							icon={recycleBin.icon}
-						/>
-					</div>
-					<div className='hazardous-waste-bin'>
-						<DisposalBin
-							content={`${hazardousWasteBin.name} Bin`}
-							direction='top'
-							icon={hazardousWasteBin.icon}
-						/>
-					</div>
-					<div className='compost-bin'>
-						<DisposalBin
-							content={`${compostBin.name} Bin`}
-							direction='top'
-							icon={compostBin.icon}
-						/>
-					</div>
-				</div>
-			</div>
+
+			<DragDropContext>
+				<Droppable droppableId='gango'>
+					{(provided) => (
+						<div
+							{...provided.droppableProps}
+							ref={provided.innerRef}>
+							{selectedItem.length > 0 && !isCarouselOpen && (
+								<Draggable
+									key='selected-item'
+									draggableId='selected-item'
+									index={6}>
+									{(provided) => (
+										<div
+											{...provided.draggableProps}
+											{...provided.dragHandleProps}
+											ref={provided.innerRef}
+											className='absolute item-picker w-36 h-36 flex flex-col items-center justify-center'>
+											<img
+												src={selectedItem[0].image}
+												alt='item'
+												className='w-100 h-auto'
+											/>
+											<span className='text-white text-sm'>
+												{selectedItem[0].name}
+											</span>
+										</div>
+									)}
+								</Draggable>
+							)}
+
+							<Draggable
+								draggableId='bulk-pickup'
+								key='bulk-pickup'
+								index={0}
+								isDragDisabled>
+								{(provided) => (
+									<div
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										ref={provided.innerRef}
+										className='absolute bulk-pickup-track max-w-280'>
+										<DisposalBin
+											content={`${bulkPickup.name} Truck`}
+											direction='top'
+											icon={bulkPickup.icon}
+										/>
+									</div>
+								)}
+							</Draggable>
+
+							<Draggable
+								draggableId='repurpose-box'
+								key='repurpose-box'
+								index={1}
+								isDragDisabled>
+								{(provided) => (
+									<div
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										ref={provided.innerRef}
+										className='repurpose-box max-w-280 absolute bottom-3'>
+										<div className='w-3/4 -mt-10'>
+											<DisposalBin
+												content={`${repurposeBin.name} Box`}
+												direction='top'
+												icon={repurposeBin.icon}
+											/>
+										</div>
+									</div>
+								)}
+							</Draggable>
+
+							<Draggable
+								key='trash-bin'
+								draggableId='trash-bin'
+								index={2}
+								isDragDisabled>
+								{(provided) => (
+									<div
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										ref={provided.innerRef}
+										className='trash-bin max-w-280 absolute bottom-3'>
+										<DisposalBin
+											content={`${trashBin.name} Bin`}
+											direction='top'
+											icon={trashBin.icon}
+										/>
+									</div>
+								)}
+							</Draggable>
+
+							<Draggable
+								key='recycle-bin'
+								draggableId='recycle-bin'
+								index={3}
+								isDragDisabled>
+								{(provided) => (
+									<div
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										ref={provided.innerRef}
+										className='recycle-bin max-w-280 absolute bottom-3'>
+										<DisposalBin
+											content={`${recycleBin.name} Bin`}
+											direction='top'
+											icon={recycleBin.icon}
+										/>
+									</div>
+								)}
+							</Draggable>
+
+							<Draggable
+								key='hazardous-waste-bin'
+								draggableId='hazardous-waste-bin'
+								index={4}
+								isDragDisabled>
+								{(provided) => (
+									<div
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										ref={provided.innerRef}
+										className='hazardous-waste-bin max-w-280 absolute bottom-3'>
+										<DisposalBin
+											content={`${hazardousWasteBin.name} Bin`}
+											direction='top'
+											icon={hazardousWasteBin.icon}
+										/>
+									</div>
+								)}
+							</Draggable>
+
+							<Draggable
+								key='compost-bin'
+								draggableId='compost-bin'
+								index={5}
+								isDragDisabled>
+								{(provided) => (
+									<div
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										ref={provided.innerRef}
+										className='compost-bin max-w-280 absolute bottom-3'>
+										<DisposalBin
+											content={`${compostBin.name} Bin`}
+											direction='top'
+											icon={compostBin.icon}
+										/>
+									</div>
+								)}
+							</Draggable>
+							{provided.placeholder}
+						</div>
+					)}
+				</Droppable>
+			</DragDropContext>
+
 			<div className='w-full absolute bottom-5 left-0 -ml-32 ground'></div>
 			<div className='absolute bottom-4 tree'>
 				<img
