1cdcf783295e9b2691be691f45ea02100f6cd3fd
Galiwango Ananiya <galiwango101@gmail.com>
2023-01-03 22:30:45 +0300

    add more attributes to the disposalMethods objects

--- src/data/data.js
+++ src/data/data.js
@@ -33,40 +33,46 @@ import WineBottle from './images/ItemWineBottle.svg';
 
 export const disposalMethods = [
 	{
+		id: 'bulk-pickup-truck',
 		name: 'Bulk Pickup',
 		description:
 			'Bulky waste means large items of solid waste such as household appliances, furniture, large auto parts, trees, branches, stumps, and other oversize wastes whose large size precludes or complicates their handling by normal solid wastes collection, processing, or disposal methods.',
 		icon: BulkPickupTruckIcon,
 	},
 	{
-		name: 'Compost',
-		description:
-			'Composting is a controlled, aerobic (oxygen-required) process that converts organic materials into a nutrient-rich soil amendment or mulch through natural decomposition.',
-		icon: CompostBinIcon,
-	},
-	{
-		name: 'Hazardous Waste',
-		description:
-			'Hazardous waste is waste that has substantial or potential threats to public health or the environment. Hazardous waste is a type of dangerous goods. Many hazardous wastes can be recycled safely and effectively, while other wastes will be treated and disposed of in landfills or incinerators.',
-		icon: HazardousWasteBinIcon,
-	},
-	{
-		name: 'Recycle',
-		description:
-			'Recycling is the process of converting waste materials into new materials and objects. The recovery of energy from waste materials is often included in this concept. The recyclability of a material depends on its ability to reacquire the properties it had in its original state.',
-		icon: RecycleBinIcon,
-	},
-	{
+		id: 'repurpose-box',
 		name: 'Repurpose',
 		description:
 			'Repurposing is the process by which an object with one use value is transformed or redeployed as an object with an alternative use value.',
 		icon: RepurposeBoxIcon,
 	},
 	{
+		id: 'trash-bin',
 		name: 'Trash',
 		description:
 			'Trashing is the process by which an object with one use value is transformed or redeployed as an object with an alternative use value.',
 		icon: TrashBinIcon,
+	},
+	{
+		id: 'recycle-bin',
+		name: 'Recycle',
+		description:
+			'Recycling is the process of converting waste materials into new materials and objects. The recovery of energy from waste materials is often included in this concept. The recyclability of a material depends on its ability to reacquire the properties it had in its original state.',
+		icon: RecycleBinIcon,
+	},
+	{
+		id: 'hazardous-waste-bin',
+		name: 'Hazardous Waste',
+		description:
+			'Hazardous waste is waste that has substantial or potential threats to public health or the environment. Hazardous waste is a type of dangerous goods. Many hazardous wastes can be recycled safely and effectively, while other wastes will be treated and disposed of in landfills or incinerators.',
+		icon: HazardousWasteBinIcon,
+	},
+	{
+		id: 'compost-bin',
+		name: 'Compost',
+		description:
+			'Composting is a controlled, aerobic (oxygen-required) process that converts organic materials into a nutrient-rich soil amendment or mulch through natural decomposition.',
+		icon: CompostBinIcon,
 	},
 ];
 
