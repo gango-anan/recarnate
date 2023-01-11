8e598627ed74552000a67f450f162f08063c7f65
Galiwango Ananiya <galiwango101@gmail.com>
2023-01-11 15:42:27 +0300

    refactor GameCommandButton to resize and reposition the button icon

--- src/components/GameCommandButton.jsx
+++ src/components/GameCommandButton.jsx
@@ -11,12 +11,21 @@ const GameCommandButton = ({
 	return (
 		<button
 			onClick={closeModal}
-			className={`${btnColor} rounded-full ${btnShadow} w-14 h-14 flex items-center justify-center`}>
-			<img
-				src={icon}
-				alt={btnInfo}
-				className='w-100 h-auto'
-			/>
+			className={`${btnColor} rounded-full ${btnShadow} flex items-center justify-center p-4`}>
+			<span
+				className='w-9 h-9'
+				style={{
+					backgroundImage: `url(${icon})`,
+					backgroundSize: 'contain',
+					backgroundRepeat: 'no-repeat',
+					backgroundPosition: 'center',
+				}}>
+				<img
+					src={icon}
+					alt={btnInfo}
+					style={{ display: 'none' }}
+				/>
+			</span>
 		</button>
 	);
 };
