f074829c57dae81317d13fa9176024133b5395f3
Galiwango Ananiya <galiwango101@gmail.com>
2023-01-12 13:29:43 +0300

    refactor to add spacing and interactivity effects to the buttons on the Exit Game prompt

--- src/pages/GameEndPromptModal.jsx
+++ src/pages/GameEndPromptModal.jsx
@@ -9,18 +9,18 @@ const GameEndPromptModal = ({ isPromptOn, closeGameExitPrompt }) => {
 	return (
 		<div className='w-screen h-screen bg-modal-background z-30 fixed left-0 top-0 flex flex-col justify-center items-center'>
 			<div className='w-365 h-336 rounded-2xl'>
-				<div className='flex flex-col gap-5 items-center game-prompt-container w-full h-full rounded-2xl pt-10 px-10'>
+				<div className='flex flex-col gap-8 items-center game-prompt-container w-full h-full rounded-2xl pt-10 px-10'>
 					<p className='recarnate-neutral-800 text-3xl font-bold text-center'>
 						Are you sure you want to end the game?
 					</p>
 					<Link
 						to='/'
-						className='block end-game-btn text-recarnate-neutral-100 font-semibold text-2xl py-3 px-8'>
+						className='block end-game-btn text-recarnate-neutral-100 font-semibold text-2xl py-3 px-8 rounded-3xl'>
 						Yes, End Game
 					</Link>
 					<button
 						onClick={closeGameExitPrompt}
-						className='block continue-game-btn text-recarnate-neutral-100 font-semibold text-2xl py-3 px-8'>
+						className='block continue-game-btn text-recarnate-neutral-100 font-semibold text-2xl py-3 px-8 rounded-3xl'>
 						No, Continue Game
 					</button>
 				</div>
